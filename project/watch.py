e48ad8e7e5076bc38994240792b291b4b52a884a
Xingzhong <xu.xeno@gmail.com>
2012-09-27 15:57:17 -0400

    update the db

--- project/watch.py
+++ project/watch.py
@@ -4,9 +4,11 @@ import pprint
 print "hello"
 pp = pprint.PrettyPrinter(indent=2)
 data = t.loadData()
+counter = 0
 for k, y in data.iteritems():
     if len( y['items'] )>0:
+        counter = counter + 1
         print k
         pp.pprint (y)
 #pp.pprint( data )
-print len(data)
+print "%s in %s"%(counter, len(data))
